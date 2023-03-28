08e8d964bcd271f1019ff66a25dc42eeb3874204
Vanya Sachkovski <ivan.sachkovski@gmail.com>
2023-03-29 02:22:27 +0300

    [GimmeHoney]: update model loader

--- sources/_gimme_honey.py
+++ sources/_gimme_honey.py
@@ -18,6 +18,8 @@ game_settings_model = {
         "machine_id": 26433
     },
     "common": {
+        "max_reels_size": [7, 6, 6, 6, 6, 7],
+        "scatters_to_fs": [0, 0, 0, 6, 8, 10, 12]
     },
     "normal_bet": {
         "paid_spins": {
@@ -47,7 +49,7 @@ game_settings_model = {
                     "weights": utils_array.get_array_horizontal(game_settings_sheet, 50, 'I', 'L'),
                 }
             ],
-            "reels_sizes_odds": [
+            "reels_size_odds": [
                 {
                     "values": utils_array.get_array_horizontal(game_settings_sheet, 46, 'Q', 'V'),
                     "weights": utils_array.get_array_horizontal(game_settings_sheet, 47, 'Q', 'V'),
@@ -83,7 +85,7 @@ game_settings_model = {
                 "values": utils_array.get_array_horizontal(game_settings_sheet, 57, 'C', 'F'),
                 "weights": utils_array.get_array_horizontal(game_settings_sheet, 58, 'C', 'F'),
             },
-            "reels_sizes_odds": [
+            "reels_size_odds": [
                 {
                     "values": utils_array.get_array_horizontal(game_settings_sheet, 57, 'Q', 'V'),
                     "weights": utils_array.get_array_horizontal(game_settings_sheet, 58, 'Q', 'V'),
@@ -123,13 +125,74 @@ parser_base.init('ngs_gimme_the_honey', '200993')
 
 parser_base.create_game_settings_file(game_settings_model)
 
-parser_base.create_strip_file('0_paid_spins.strip_set', strips_sheet,
+parser_base.create_strip_file('0_paid.strip_set', strips_sheet,
                               ['C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
                                'U', 'V', 'W', 'X', 'Y', 'Z'],
                               3, 142,
                               utils_transform.transform_inc)
-parser_base.create_strip_file('1_free_spins.strip_set', strips_sheet,
+
+parser_base.create_strip_file('1_free.strip_set', strips_sheet,
                               ['AD', 'AE', 'AF', 'AG', 'AH', 'AI', 'AJ', 'AK', 'AL', 'AM', 'AN', 'AO', 'AP', 'AQ', 'AR',
                                'AS', 'AT', 'AU', 'AV', 'AW', 'AX', 'AY', 'AZ', 'BA'],
                               3, 142,
+                              utils_transform.transform_inc)
+
+parser_base.create_strip_file('2_paid_second_chance.strip_set', strips_sheet,
+                              ['BF', 'BG', 'BH', 'BI', 'BJ', 'BK', 'BL', 'BM', 'BN', 'BO', 'BP', 'BQ'],
+                              3, 142,
+                              utils_transform.transform_inc)
+
+parser_base.create_strip_file('3_free_second_chance.strip_set', strips_sheet,
+                              ['BU', 'BV', 'BW', 'BX', 'BY', 'BZ', 'CA', 'CB', 'CC', 'CD', 'CE', 'CF'],
+                              3, 142,
+                              utils_transform.transform_inc)
+
+parser_base.create_strip_file('4_mystery.strip_set', strips_sheet,
+                              ['CK', 'CL', 'CM', 'CN', 'CO', 'CP', 'CQ', 'CR', 'CS', 'CT', 'CU', 'CV'],
+                              3, 142,
+                              utils_transform.transform_inc)
+
+parser_base.create_strip_file('5_paid_queen_wild.strip_set', strips_sheet,
+                              ['DA', 'DB', 'DC', 'DD', 'DE', 'DF', 'DG', 'DH', 'DI', 'DJ', 'DK', 'DL'],
+                              3, 142,
+                              utils_transform.transform_inc)
+
+parser_base.create_strip_file('6_free_queen_wild.strip_set', strips_sheet,
+                              ['DP', 'DQ', 'DR', 'DS', 'DT', 'DU', 'DV', 'DW', 'DX', 'DY', 'DZ', 'EA'],
+                              3, 142,
+                              utils_transform.transform_inc)
+
+parser_base.create_strip_file('0_paid.bottom_tracker', strips_sheet,
+                              ['C', 'D', 'E', 'F', 'A', 'A'],
+                              147, 266,
+                              utils_transform.transform_inc)
+
+parser_base.create_strip_file('1_free.bottom_tracker', strips_sheet,
+                              ['AD', 'AE', 'AF', 'AG', 'A', 'A'],
+                              147, 266,
+                              utils_transform.transform_inc)
+
+parser_base.create_strip_file('2_paid_second_chance.bottom_tracker', strips_sheet,
+                              ['BF', 'BG', 'A', 'A', 'A', 'A'],
+                              147, 266,
+                              utils_transform.transform_inc)
+
+parser_base.create_strip_file('3_free_second_chance.bottom_tracker', strips_sheet,
+                              ['BU', 'BV', 'A', 'A', 'A', 'A'],
+                              147, 266,
+                              utils_transform.transform_inc)
+
+parser_base.create_strip_file('4_mystery.bottom_tracker', strips_sheet,
+                              ['CK', 'CL', 'A', 'A', 'A', 'A'],
+                              147, 266,
+                              utils_transform.transform_inc)
+
+parser_base.create_strip_file('5_paid_queen_wild.bottom_tracker', strips_sheet,
+                              ['DA', 'DB', 'A', 'A', 'A', 'A'],
+                              147, 266,
+                              utils_transform.transform_inc)
+
+parser_base.create_strip_file('6_free_queen_wild.bottom_tracker', strips_sheet,
+                              ['DP', 'DQ', 'A', 'A', 'A', 'A'],
+                              147, 266,
                               utils_transform.transform_inc)
