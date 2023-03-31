1cbb4bca23afa8446ac23f39f1c268fb4201b548
Vanya Sachkovski <ivan.sachkovski@gmail.com>
2023-04-01 01:59:16 +0300

    [Core]: optimize methods

--- sources/utils_array.py
+++ sources/utils_array.py
@@ -9,75 +9,57 @@ def get_array_vertical(sheet,
     Get as array column `col_name` from `sheet` starting from `start_index_row`.
     Apply `transform_func` to each element.
     """
-    transform_func = utils_transform.transform_none if transform_func is None else transform_func
+    assert start_row_index > 0
+    start_row_index -= 1
 
-    out_array = []
-    column = sheet[col_name]
+    transform_func = transform_func or utils_transform.transform_none
+    column = sheet[col_name][start_row_index:]
 
-    index = 0
-    for cell in column:
-        index += 1
-
-        if index < start_row_index:
-            continue
-
-        if cell.value is None:
-            break
-
-        value = transform_func(cell.value)
-        out_array.append(value)
-
-    return out_array
+    return [transform_func(cell.value) for cell in column if cell.value is not None]
 
 
 def get_complex_array_vertical(sheet,
                                col_names: list,
                                start_row_index: int,
-                               end_row_index=None,
-                               separator='|',
+                               end_row_index: int = None,
+                               separator: str = '|',
                                transform_func=None):
     """
     Get as array joined (via `separator`) columns belongs to `col_names` from `sheet` starting from `start_index_row`.
     """
-    transform_func = utils_transform.transform_none if transform_func is None else transform_func
+    assert start_row_index > 0
+    start_row_index -= 1
 
-    out_array = []
+    transform_func = transform_func or utils_transform.transform_none
+
+    assert len(col_names) > 0
+    max_num_rows = len(sheet[col_names[0]][start_row_index:end_row_index])
+    out_array = ['' for _ in range(max_num_rows)]
 
     for col_name in col_names:
 
-        index = 0
-        column = sheet[col_name]
+        index = -1
+        column = sheet[col_name][start_row_index:end_row_index]
 
         for cell in column:
             index += 1
-
-            # Do not process rows before the start one
-            if index < start_row_index:
-                continue
-
-            # Do not process rows after the last one (if it configured)
-            if end_row_index is not None and index > end_row_index:
-                break
-
-            output_index = index - start_row_index
-
-            if output_index >= len(out_array):
-                out_array.append('')
 
             # Do not process cells without values
             if cell.value is None:
                 continue
 
-            if out_array[output_index] != '':
-                out_array[output_index] += separator
+            if out_array[index] != '':
+                out_array[index] += separator
 
-            out_array[output_index] += str(transform_func(cell.value))
+            out_array[index] += str(transform_func(cell.value))
 
     return out_array
 
 
-def get_array_horizontal(sheet, row: int, col_name_begin: str, col_name_end: str):
-
+def get_array_horizontal(sheet,
+                         row: int,
+                         col_name_begin: str,
+                         col_name_end: str):
     range_begin = col_name_begin + str(row)
     range_end = col_name_end + str(row)
 
--- sources/utils_transform.py
+++ sources/utils_transform.py
@@ -1,12 +1,9 @@
 
 def transform_none(value):
-    """
-    Do not apply any transform to value.
-    """
     return value
 
 
-def transform_remove_first(value):
+def transform_remove_first(value: str):
     """
     Remove the first symbol in value.
     E.g. `x03330` -> '03330'.
@@ -14,8 +11,5 @@ def transform_remove_first(value):
     return value[1:]
 
 
-def transform_inc(value):
-    """
-    Do not apply any transform to value.
-    """
+def transform_inc(value: int):
     return value + 1
